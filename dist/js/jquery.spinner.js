9a344a5ed8f0c96c4fa38e52111e606b0c40b81a
vsn4ik <vsn4ik@gmail.com>
2017-01-07 05:08:17 +0300

    Update jquery and qunit

--- dist/js/jquery.spinner.js
+++ dist/js/jquery.spinner.js
@@ -1,6 +1,6 @@
 /*!
  * jquery.spinner v0.2.1 (https://vsn4ik.github.io/jquery.spinner/)
- * Copyright 2013-2016 xixilive
+ * Copyright 2013-2017 xixilive
  * Licensed under the MIT license
  */
 'use strict';
